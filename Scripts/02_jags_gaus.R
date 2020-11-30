918e19574d65cc7af8431fb61e7c36302e480af6
emhedlin <hedlin@ualberta.ca>
2020-11-30 16:51:12 -0700

    fixed dimension inconsistencies, model runs

--- Scripts/02_jags_gaus.R
+++ Scripts/02_jags_gaus.R
@@ -1,6 +1,7 @@
 # We take jags_gaus.R another step. To investigate correlation between
 # slope and intercepts in year, we want to nest random slopes
 # for chicks + chickage in random intercept for year
+# https://people.ucsc.edu/~abrsvn/general-random-effects-jags.html
 
 
 library(R2jags)
@@ -30,6 +31,9 @@ dim(d)
 n_nests <- length(levels(d$site))
 n_yearsites <- length(levels(d$yearsite_f))
 n_years <- length(levels(d$year))
+
+
+# Jags model / run --------------------------------------------------------
 
 
 jags_data <- list(y           = d$logIVI,    # ivi 
@@ -88,17 +92,24 @@ cat("
             b_chicks[j]   <- xi[3] * B_raw[j, 3]
         }  
     
-      # homogenous residuals
-        sigma_res ~ dunif(0, 100) # Residual standard deviation
-        tau_res   <- 1 / (sigma_res * sigma_res)
-    
     
       # Likelihood ~~~~~~~~~~~~~~~~~~~~~~~~~~~
          for (i in 1:N) {
-         mu_obs[i] <- a_year[years[i]] + b_chickage[years[i]] * chickage + b_chicks[years[i]] * chicks
-         y[i] ~ dnorm(mu_obs[i], tau_res)
+         mu_obs[i] <- a_year[years[i]] + b_chickage[years[i]] * chickage[i] + b_chicks[years[i]] * chicks[i]
+         y[i] ~ dnorm(mu_obs[i], tau[years[i]])
          }
 
+      # homogenous residuals - het residuals defined on line 132
+       # sigma_res ~ dunif(0, 100) # Residual standard deviation
+       # tau_res   <- 1 / (sigma_res * sigma_res)
+    
+     # het residuals - prior for residual variance weighting matrix
+        for (i in 1:n_years){
+        chSq[i]  ~ dgamma(0.5, 0.5)
+        z[i]     ~ dnorm(0, 0.04)I(0,)
+        sigma_resid[i] <- z[i] / sqrt(chSq[i])
+        tau[i]   <- pow(sigma_resid[i], -2)
+        }
     
     # Priors ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
         for (i in 1:3) {
@@ -108,8 +119,8 @@ cat("
         }
         
         mu_int_prior    <- mu_prior[1]
-        mu_slope1_prior <- mu_prior[2]
-        mu_slope2_prior <- mu_prior[3]
+        mu_chickage_prior <- mu_prior[2]
+        mu_chicks_prior <- mu_prior[3]
         Tau_B_raw_prior[1:3, 1:3] ~ dwish(W[,], 4)
         
         Sigma_B_raw_prior[1:3, 1:3] <- inverse(Tau_B_raw_prior[,])
@@ -129,13 +140,6 @@ cat("
         rho_int_chicks_prior      <- rho_prior[1, 3]
         rho_chickage_chicks_prior <- rho_prior[2, 3]
 
-     # het residuals - prior for residual variance weighting matrix
-        # for (i in 1:n_years){
-        # chSq[i]  ~ dgamma(0.5, 0.5)
-        # z[i]     ~ dnorm(0, 0.04)I(0,)
-        # sigma_resid[i] <- z[i] / sqrt(chSq[i])
-        # tau[i]   <- pow(sigma_resid[i], -2)
-        # }
     }
 ", fill = TRUE)
 sink()
@@ -169,10 +173,8 @@ params <- c("mu",
             "rho_int_chickage", 
             "rho_int_chicks", 
             "rho_chickage_chicks", 
-            "alpha", 
             "b_chickage", 
             "b_chicks", 
-            "sigma_res", 
             "mu_int_prior", 
             "mu_chickage_prior", 
             "mu_chicks_prior", 
@@ -193,3 +195,180 @@ het_m1   <- jags(data      = jags_data,
                  n.chains   = 3,
                  n.burnin   = 4000,
                  n.iter     = 7000)
+
+het_m2 <- update(het_m1, n.iter = 10000, n.thin = 10) 
+het_m3 <- update(het_m2, n.iter = 20000, n.thin = 10) 
+het_m4 <- update(het_m3, n.iter = 20000, n.thin = 10) 
+
+
+
+
+
+
+# Trace Plots -------------------------------------------------------------
+
+## Parameters ##
+# sigma_resid - yearly residual variance
+
+# off diagonal covariances between ranefs
+# rho[1,3]
+# rho[1,2]
+# rho[2,1]
+# rho[2,3]
+# rho[3,1]
+# rho[3,2]
+
+# rho_int_chickage
+# rho_int_chicks
+# rho_chickage_chicks
+
+het_mcmc <- as.mcmc(het_m3) #change back to 4 later 
+
+
+het_mcmc %>%
+  window(thin=10) %>% 
+  
+  # sigma = the yearly residual variance, a = random intercept for site, g = random intercept for yearsite
+  tidybayes::gather_draws(b_chickage[i], b_chicks[i], rho_int_chickage, rho_int_chicks) %>%
+  
+  # change this filter to look at mixing for the parameter of interest. See above line for options
+  filter(.variable == "rho_int_chicks") %>% 
+  ungroup() %>%
+  mutate(term = ifelse(is.na(i), .variable, paste0(.variable,"[",i,"]"))) %>%
+  ggplot(aes(x=.iteration, y=.value, color=as.factor(.chain))) +
+  scale_color_manual(values=c("#461220", "#b23a48", "#fcb9b2")) +
+  geom_line(alpha=0.5) +
+  facet_grid(term~., scale="free_y") +
+  labs(color="chain", x="iteration") +
+  theme_nuwcru()
+
+
+
+# Rhat --------------------------------------------------------------------
+
+
+rhat(het_mcmc) %>% 
+  data.frame() %>% 
+  rownames_to_column() %>% 
+  set_names("parameter", "rhat") %>% 
+  filter(parameter != "lp__") %>% 
+  
+  ggplot(aes(x = rhat, y = reorder(parameter, rhat))) + 
+  geom_segment(aes(xend = 1, yend = parameter),
+               color = "#EEDA9D") +
+  geom_point(aes(color = rhat > 1), 
+             size = 2) +
+  scale_color_manual(values = c("#80A0C7", "#A65141")) +
+  labs(x = NULL, y = NULL) +
+  theme_nuwcru() +
+  theme(legend.position = "none",
+        axis.ticks.y    = element_blank(),
+        axis.text.y     = element_text(hjust = 0))
+
+
+
+# Ranef Covariance --------------------------------------------------------
+
+
+posterior_samples(het_mcmc) %>%
+  ggplot() +
+  geom_density(aes(x = cor_year__Intercept__chickage),
+               color = "transparent", fill = blue2, alpha = 5/10) +
+  geom_density(aes(x = cor_year__Intercept__chicks),
+               color = "transparent", fill = red2, alpha = 5/10) +
+  annotate(geom = "text", x = 0.6, y = 1.75, 
+           label = "a_year | B_chickage", color = blue2, family = "Courier") +
+  annotate(geom = "text", x = 0.6, y = 2, 
+           label = "a_year | B_chicks", color = red2, family = "Courier") +
+  scale_y_continuous(limits = c(0, 2.5)) +
+  labs(subtitle = "Posterior distributions for correlations\nbetween intercepts and slopes",
+       x = "correlation") +
+  theme_nuwcru()
+
+
+# Parm Estimates ----------------------------------------------------------
+
+
+
+
+# Convert to data to be exported if needed ~~~~~~~~~~~~~~~~~~~~~~~
+
+x <- het_mcmc %>%
+  window(thin=10) %>% 
+  # sigma = the yearly residual variance, a = random intercept for site, g = random intercept for yearsite
+  tidybayes::gather_draws(beta[i], sigma[i], g[i])
+
+
+beta <- x %>% filter(.variable == "beta") %>% mutate(i = as.factor(i))
+for (i in 1:length(unique(beta$i))){levels(beta$i)[i] <- colnames(X)[i]}
+levels(beta$i)[1] <- "intercept"
+
+sigma <- x %>% filter(.variable == "sigma") %>% mutate(i = as.factor(i))
+for (i in 1:length(unique(sigma$i))){levels(sigma$i)[i] <- paste0("sigma", "_", levels(d$year)[i])}
+
+# a <- x %>% filter(.variable == "a") %>% mutate(i = as.factor(i))
+# for (i in 1:length(unique(a$i))){levels(a$i)[i] <- unique(as.character(d$site))[i]}
+
+g <- x %>% filter(.variable == "g") %>% mutate(i = as.factor(i))
+for (i in 1:length(unique(g$i))){levels(g$i)[i] <- unique(as.character(d$yearsite))[i]}
+
+# year <- x %>% filter(.variable == "year") %>% mutate(i = as.factor(i))
+# for (i in 1:length(unique(year$i))){levels(year$i)[i] <- paste0("int", "_", levels(d$year)[i])}
+# unique(year$i)
+
+all <- rbind(beta, sigma, g)
+names(all) <- c("level", "chain", "iteration", "draw", "parameter", "value")
+
+
+
+arrow::write_parquet(all, "data/jags_out_gamma.parquet")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+# Posterior Prediction ----------------------------------------------------
+
+getmode <- function(x) {
+  uniqx <- unique(x)
+  uniqx[which.max(tabulate(match(x, uniqx)))]
+}
+
+
+
+m1 <-
+  coef(fit1)$year[ , 1, 1:3] %>%
+  as_tibble() %>%               
+  mutate(year = 2013:2019) %>%  
+  select(year, everything())    
+
+
+d %>%
+  tidyr::expand(chickage = 1:12, chicks = 1:4, year = 2013:2019) %>%
+  tidybayes::add_predicted_draws(fit1, n = 100) %>%
+  group_by(chickage, year) %>%
+  summarize(mode = mean(.prediction),
+            sd = sd(.prediction)) %>%
+  mutate(upper95 = mode + (sd*1.96),
+         upper80 = mode + (sd*1.282),
+         upper50 = mode + (sd*0.674),
+         lower95 = mode - (sd*1.96),
+         lower80 = mode - (sd*1.282),
+         lower50 = mode - (sd*0.674)) %>%
+  ggplot() +
+  geom_ribbon(aes(x = chickage, ymin = lower95, ymax = upper95), fill = red5, alpha = 0.4) +
+  geom_ribbon(aes(x = chickage, ymin = lower80, ymax = upper80), fill = red4, alpha = 0.5) +
+  geom_ribbon(aes(x = chickage, ymin = lower50, ymax = upper50), fill = red3, alpha = 0.8) +
+  geom_line(aes(x = chickage, y = mode), colour = red1) +
+  facet_grid(~year) +
+  theme_nuwcru() 
+
+
+
+
+
