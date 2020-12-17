c4773ee78bdbb2ee7eb60dc3c06f49a3aa360bbb
emhedlin <hedlin@ualberta.ca>
2020-12-17 16:46:51 -0700

    edited brms to use cmdstanr as back end, dev residual

--- Scripts/brms.R
+++ Scripts/brms.R
@@ -6,7 +6,9 @@
 
 
 install.packages("tictoc")
-install.packages("commandstanr")
+install_cmdstan()
+set_cmdstan_path()
+library(cmdstanr)
 library(R2jags)
 library(nuwcru)
 library(dplyr)
@@ -35,7 +37,7 @@ d <- read_csv("Data/ivi_eh.csv") %>%
 d <- d %>% filter(!is.na(chicks) & !is.na(chickage) & !is.na(logIVI))
 
 # add lat long locations
-d <- read_csv("/Volumes/GoogleDrive/My Drive/NuWCRU/Analysis/emhedlin/pefa.surv/data/sites.csv") %>% 
+d <- read_csv("Data/sites.csv") %>% 
   select(site = SiteID, lat = SiteLatitudeDD , long = SiteLongitudeDD) %>%
   mutate(site = as.factor(site)) %>% 
   right_join(d, by = "site")
@@ -49,19 +51,35 @@ d <- read_csv("/Volumes/GoogleDrive/My Drive/NuWCRU/Analysis/emhedlin/pefa.surv/
 # using the log transformed to keep things simple
 
 tictoc::tic()
-fit1 <- brm(logIVI ~ 1 +  (1 + chicks + chickage | year ),
+m1_cor <- brm(logIVI ~ 1 +  (1 + chicks + chickage | year ),   # linear model with random slopes for chicks and chickage nested in year
             data = d, 
-            # you can delete this prior section, brms will automatically pick some uninformative priors for you
             prior = c(#set_prior("normal(0,5)", class = "b"),
-                      set_prior("cauchy(0,2)", class = "sd"),
-                      set_prior("lkj(2)", class = "cor")),
+                      set_prior("lkj(2)", class = "cor"),    # delete this line if you don't want to estimate correlation among ranef
+                      set_prior("cauchy(0,2)", class = "sd")),
             warmup = 1000, 
             iter = 5000, 
             chains = 4,
             control = list(adapt_delta = 0.98),
-            backend = "cmdstanr",
+            backend = "cmdstanr", 
             cores = 8)
 tictoc::toc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 fit2 <- brm(logIVI ~ 1 +  chickage + (1 + chicks + chickage | year ),
             data = d, 
@@ -87,46 +105,70 @@ fit2 <- brm(logIVI ~ 1 + (1 + chicks + chickage | year ),
             control = list(adapt_delta = 0.98),
             cores = 8)
 
-waic(fit2)
+waic(m1_cor)
+
+
 
 # examine residuals ~~~~~~~~~~~~~~~~~~~~~~~~~
 # work in progress
 
-x <- d %>%
-  add_residual_draws(fit1) %>%
-  ggplot(aes(x = .row, y = .residual)) +
-  facet_grid(~year) +
-  stat_pointinterval() +
-  theme_nuwcru() + 
-  facet_nuwcru()
-    
 
+x <- cbind(d, resids)
 
-
-
-
-
-
-loc <- read_csv("/Volumes/GoogleDrive/My Drive/NuWCRU/Analysis/emhedlin/pefa.surv/data/sites.csv") %>% 
-  select(site = SiteID, lat = SiteLatitudeDD , long = SiteLongitudeDD) %>%
-  mutate(site = as.factor(site))
-
-nd <- nd %>% left_join(loc, by = "site")
-
-# checking for spatial patterns in resids
-nd %>% group_by(site, lat, long) %>% summarize(mean_resid = mean(abs(Estimate))) %>%
+# something very wrong about 2019. That diagnoal line indicates an issue in the data. Error perfectly scales with logIVI in some case
+x %>%
   ggplot() +
-  geom_point(aes(x = long, y = lat, size = mean_resid), shape = 21) +
-  # facet_grid(~year) +
-  theme_nuwcru() + facet_nuwcru()
+  geom_point(aes(x = logIVI, y = Estimate), alpha = 0.2, colour = blue2) +
+  facet_grid(~year) +
+  theme_nuwcru() +
+  facet_nuwcru()
 
+# list the sites in 2019
+x %>% filter(year == "2019") %>% distinct(site)
 
+# we can see there's an issue with site 8, this is not a normal pattern
+x %>% filter(year == "2019") %>%
+  ggplot() +
+  geom_point(aes(x = logIVI, y = Estimate), colour = grey6) +
+  geom_point(data = filter(x, site == 8 & year == "2019"), aes(x = logIVI, y = Estimate), colour = blue2) +
+  facet_grid(~year) +
+  theme_nuwcru() +
+  facet_nuwcru()
 
+# let's fit a linear model to every site. If we get large absurdly large p values, we'll
+# know there's a strange pattern associated with that site.
+x %>% filter(year == "2019") %>%
+  group_by(site) %>%
+  do(fit_resid = broom::tidy(lm(logIVI ~ Estimate, data = .))) %>% 
+  unnest(fit_resid) %>%
+  filter(term == "Estimate")
+
+# from the above, we can see sites 8 and 151 have incredible tight linear patterns
+# between the logIVI and the model residuals. This isn't right. let's plot both sites
+x %>% filter(year == "2019") %>%
+  ggplot() +
+  geom_point(aes(x = logIVI, y = Estimate), colour = grey6) +
+  geom_point(data = filter(x, site == 151 & year == "2019"), aes(x = logIVI, y = Estimate), alpha = 0.5, colour = red2) +
+  geom_point(data = filter(x, site == 8 & year == "2019"), aes(x = logIVI, y = Estimate), alpha = 0.5, colour = blue2) +
+  facet_grid(~year) +
+  theme_nuwcru() +
+  facet_nuwcru()
+
+# logIVI by date, highlight the problem sites
+x %>% filter(year == "2019") %>%
+  ggplot() +
+  geom_point(aes(y = logIVI, x = date), colour = grey6) +
+  geom_point(data = filter(x, site == 151 & year == "2019"), aes(y = logIVI, x = date), colour = red2) +
+  geom_point(data = filter(x, site == 8 & year == "2019"), aes(y = logIVI, x = date), colour = blue2) +
+  theme_nuwcru()
+
+d %>%
+  add_fitted_draws(m1_cor) 
 
 
 # trace plots ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-posterior_samples(fit1, add_chain = T) %>% 
+posterior_samples(m1_cor, add_chain = T) %>% 
   select(-lp__) %>% 
   gather(key, value, -chain, -iter) %>% 
   mutate(chain = as.character(chain)) %>% 
@@ -145,7 +187,7 @@ posterior_samples(fit1, add_chain = T) %>%
 
 # Rhat
 
-rhat(fit1) %>% 
+rhat(m1_cor) %>% 
   data.frame() %>% 
   rownames_to_column() %>% 
   set_names("parameter", "rhat") %>% 
@@ -170,7 +212,7 @@ rhat(fit1) %>%
 # Covariance between intercept and slopes ---------------------------------
 
 
-posterior_samples(fit1) %>%
+posterior_samples(m1_cor) %>%
   ggplot() +
   geom_density(aes(x = cor_year__Intercept__chickage),
               color = "transparent", fill = blue2, alpha = 5/10) +
@@ -187,7 +229,7 @@ posterior_samples(fit1) %>%
 
 
 
-ranef(fit1)
+
 
 
 # Posterior prediction ----------------------------------------------------
@@ -196,18 +238,17 @@ getmode <- function(x) {
   uniqx[which.max(tabulate(match(x, uniqx)))]
 }
 
-
-m1 <-
-  coef(fit1)$year[ , 1, 1:3] %>%
+# betas
+coef(m1_cor)$year[ , 1, 1:3] %>%
   as_tibble() %>%               
   mutate(year = 2013:2019) %>%  
   select(year, everything())    
 
-
+# posterior predict
 d %>%
   tidyr::expand(chickage = 1:12, chicks = 1:4, year = 2013:2019) %>%
-  tidybayes::add_predicted_draws(fit1, n = 100) %>%
-  group_by(chickage, year) %>%
+  tidybayes::add_predicted_draws(m1_cor, n = 100) %>%
+  group_by(chicks, year) %>%
   summarize(mode = mean(.prediction),
             sd = sd(.prediction)) %>%
   mutate(upper95 = mode + (sd*1.96),
@@ -217,10 +258,10 @@ d %>%
          lower80 = mode - (sd*1.282),
          lower50 = mode - (sd*0.674)) %>%
   ggplot() +
-  geom_ribbon(aes(x = chickage, ymin = lower95, ymax = upper95), fill = red5, alpha = 0.4) +
-  geom_ribbon(aes(x = chickage, ymin = lower80, ymax = upper80), fill = red4, alpha = 0.5) +
-  geom_ribbon(aes(x = chickage, ymin = lower50, ymax = upper50), fill = red3, alpha = 0.8) +
-  geom_line(aes(x = chickage, y = mode), colour = red1) +
+  geom_ribbon(aes(x = chicks, ymin = lower95, ymax = upper95), fill = red5, alpha = 0.4) +
+  geom_ribbon(aes(x = chicks, ymin = lower80, ymax = upper80), fill = red4, alpha = 0.5) +
+  geom_ribbon(aes(x = chicks, ymin = lower50, ymax = upper50), fill = red3, alpha = 0.8) +
+  geom_line(aes(x = chicks, y = mode), colour = red1) +
   facet_grid(~year) +
   theme_nuwcru() 
 
